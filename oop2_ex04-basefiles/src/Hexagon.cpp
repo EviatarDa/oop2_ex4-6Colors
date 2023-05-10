1ccbe2430cdce9bd587aadac4727141391ac162e
ShirYadid99 <shir19991999@gmail.com>
2023-05-10 14:12:23 +0300

    save

--- oop2_ex04-basefiles/src/Hexagon.cpp
+++ oop2_ex04-basefiles/src/Hexagon.cpp
@@ -2,12 +2,18 @@
 
 #include "Hexagon.h"
 
+
 Hexagon::Hexagon(int row, int col)
 	:m_hexagon(RADIUS,6)
 {
-	//srand(time(NULL));
+	std::random_device rd;                       // Obtain a random seed from the hardware
+	std::mt19937 generator(rd());                 // Initialize the random number generator
+	std::uniform_int_distribution<int> distribution(0, 99);  // Define the range of the random numbers
+
+	int randomNumber = distribution(generator);  // Generate a random number
+
 	m_hexagon.setOrigin(0, 0);
-	m_color = Resources::instance().getColorArray()[rand() % 6];
+	m_color = Resources::instance().getColorArray()[randomNumber % 6];
 	m_hexagon.setOrigin(25, 25);
 	m_hexagon.setPosition((col % 2 ? 3 * RADIUS + OUTLINE : 2 * RADIUS + OUTLINE) + row * 2 * RADIUS,
 		2 * RADIUS + col * 1.5 * RADIUS + OUTLINE);
